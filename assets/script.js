c5acd7fb882148959a726d89f0bc24a8ff3450c4
Jason Diaz <jason@Jasons-MacBook-Air.local>
2022-01-31 20:47:39 -0700

    add functionality to the webpage

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,97 @@
-// HINT: You can delete this console.log after you no longer need it!
 console.log('JavaScript code has loaded!')
-// First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
 
-// We'll use these variables to track the counts of each cookie type
+let yourName = "Jason Diaz"
+
 let gb = 0 // Gingerbread
 let cc = 0 // Chocolate Chip
 let sugar = 0 // Sugar Sprinkle
+let total = 0 // Total Amount of Cookies
 
-// selecting the element with an id of credit
 const credit = document.querySelector('#credit')
-// selecting the element with an id of add-gb
-const gbPlusBtn = document.querySelector('#add-gb')
 
-// Code to update name display
+const gbPlusBtn = document.querySelector('#add-gb')
+const ccPlusBtn = document.querySelector('#add-cc')
+const sugarPlusBtn = document.querySelector('#add-sugar')
+
+const gbMinusBtn = document.querySelector('#minus-gb')
+const ccMinusBtn = document.querySelector('#minus-cc')
+const sugarMinusBtn = document.querySelector('#minus-sugar')
+
+let quantityTotal = document.querySelector('#qty-total')
+
+/* 
+	Error message is logged if the user pressses
+	negative if the quantity and or the total
+	is less 0 
+*/
+let quantityNegativeERR = "the quantity of item and or total cannot be less than 0"
+
 credit.textContent = `Created by ${yourName}`
 
-// Event listener for clicks on the "+" button for Gingerbread cookies
-gbPlusBtn.addEventListener('click', function() {
-// HINT: You can delete this console.log after you no longer need it!
-console.log('Gingerbread + button was clicked!')
+// Plus Button Functionality
 
-// TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+quantityGB = document.getElementById("qty-gb")
+gbPlusBtn.addEventListener('click', function() {
+	gb += 1
+	quantityGB.textContent = gb
+
+	total += 1
+	quantityTotal.textContent = total
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+quantityCC = document.getElementById("qty-cc")
+ccPlusBtn.addEventListener('click', function() {
+	cc += 1
+	quantityCC.textContent = cc
+
+	total += 1
+	quantityTotal.textContent = total
+})
+
+quantitySugar = document.getElementById("qty-sugar")
+sugarPlusBtn.addEventListener('click', function() {
+	sugar += 1
+	quantitySugar.textContent = sugar
+
+	total += 1
+	quantityTotal.textContent = total
+})
+
+// Minus Button Functionality
+
+gbMinusBtn.addEventListener('click', function() {
+	if (quantityGB.textContent == 0) {
+		console.log(quantityNegativeERR)
+	} else {
+		gb -= 1
+		quantityGB.textContent = gb
+
+		total -= 1
+		quantityTotal.textContent = total
+	}
+})
+
+ccMinusBtn.addEventListener('click', function() {
+	if (quantityCC.textContent == 0) {
+		console.log(quantityNegativeERR)
+	} else {
+		cc -= 1
+		quantityCC.textContent = cc
+
+		total -= 1
+		quantityTotal.textContent = total
+	}
+})
+
+sugarMinusBtn.addEventListener('click', function() {
+	if (quantitySugar.textContent == 0) {
+		console.log(quantityNegativeERR)
+	} else {
+		sugar -= 1
+		quantitySugar.textContent = sugar
+
+		total -= 1
+		quantityTotal.textContent = total
+	}
+	
+})
